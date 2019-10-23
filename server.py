45ccc13f7cfad153c2a3c219aec7052541d2527c
chewcw <chwai87@gmail.com>
2019-10-23 03:38:06 +0000

    first initialized

---
+++ server.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from pathlib import Path
+from flask_azure_oauth import FlaskAzureOauth
+
+# environment file
+load_dotenv(dotenv_path='.env')
+
+# application
+app = Flask(__name__)
+# for hot reloading
+app.debug = True
+# configuration
+app.config['AZURE_OAUTH_TENANCY'] = os.environ['AZURE_OAUTH_TENANCY']
+app.config['AZURE_OAUTH_APPLICATION_ID'] = os.environ['AZURE_OAUTH_APPLICATION_ID']
+app.config['AZURE_OAUTH_CLIENT_APPLICATION_IDS'] = os.environ['AZURE_OAUTH_CLIENT_ID']
+
+auth = FlaskAzureOauth()
+auth.init_app(app=app)
+
+@app.route('/api/unprotected')
+def unprotected():
+    return 'this is public route without protection'
+
+@app.route('/api/protected')
+@auth()
+def protected():
+    return 'hahahaah, this is protected route without scope specified'
+
+@app.route('/api/all')
+@auth('all')
+def test():
+	return 'hahahahah, this is protected route with scope `all` specified'
+
+if __name__ == '__main__':
+	app.run(port=8000)
